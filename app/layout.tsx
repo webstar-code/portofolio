8c63cc2544435bf5ee021b40b89e7aace5a37e9e
webstar-code <bhaveshrc2001@gmail.com>
2024-02-25 23:29:23 +0530

    chore: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "../styles/globals.css";
 import { ThemeProvider } from "@/component/providers";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Roboto_Mono({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
